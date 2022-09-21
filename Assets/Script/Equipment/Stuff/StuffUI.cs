01de18861e9c0fc1a8730f126e713a4a0f9386b2
RofaielMakram <113625627+RofaielMakram@users.noreply.github.com>
2022-09-21 14:34:18 -0700

    update stuff

--- Assets/Script/Equipment/Stuff/StuffUI.cs
+++ Assets/Script/Equipment/Stuff/StuffUI.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class StuffUI : MonoBehaviour
 {
+
    [SerializeField] GameObject stuff;
+
+    public Transform EquipParent;
+    [SerializeField] InventorySlot[] slots;
+
+    private void Start()
+    {
+       // slots = EquipParent.GetComponentInChildren<InventorySlot>();
+    }
     void Update()
     {
         if (Input.GetButtonDown("Stuff"))
