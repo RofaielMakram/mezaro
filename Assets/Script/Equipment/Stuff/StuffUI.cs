d557d014a9a9938ce2e431a005f3586bfbac0bbe
RofaielMakram <113625627+RofaielMakram@users.noreply.github.com>
2022-09-21 08:57:51 -0700

    add stuff

---
+++ Assets/Script/Equipment/Stuff/StuffUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffUI : MonoBehaviour
+{
+   [SerializeField] GameObject stuff;
+    void Update()
+    {
+        if (Input.GetButtonDown("Stuff"))
+        {
+            stuff.SetActive(!stuff.activeSelf);
+        }
+    }
+}
