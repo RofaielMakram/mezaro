df563a1d84df1a80fcb471560f38d5bae95ea060
RofaielMakram <113625627+RofaielMakram@users.noreply.github.com>
2023-01-21 09:43:24 -0800

    Add direction y into camera control in aiming and default position in (archer girl)
    -Updating (third person camera)system.

---
+++ Assets/Script/controll Camera/MouseLookY.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLookY : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float mouseSensetivity;
+    [SerializeField]
+    float xRotation = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensetivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -10f, 27f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/Script/controll Camera/MouseLookY2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLookY2 : MonoBehaviour
+{
+    [SerializeField]
+    float mouseSensetivity;
+    [SerializeField]
+    float mouseY;
+    [SerializeField]
+    float xRotation=0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensetivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
