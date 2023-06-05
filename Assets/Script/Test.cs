fca960190fa77c6b84d98ad260ae999b525e7a8a
MinaNady71 <Abanoubnady206@gmail.com>
2023-06-05 19:48:04 +0300

    222

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    
+    public GameObject activeObject;
+    public GameObject disactiveObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActiveAndDisActive();
+    }
+
+    public void ActiveAndDisActive()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+            {
+
+                activeObject.SetActive(true);
+            }
+        
+
+    }
+}
